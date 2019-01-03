fb8a5e1698fff055c813d1b832aedfbf23aca18b
Mateus Santana <mateuss.dev@gmail.com>
2019-01-02 22:38:43 -0300

    update docz version and add configs

---
+++ doczrc.js
@@ -0,0 +1,10 @@
+export default {
+  title: 'Gatsby Starter Editorial',
+  description: 'Editorial components',
+  themeConfig: {
+    mode: 'dark',
+    colors: {
+      primary: '#f56a6a',
+    },
+  },
+}
